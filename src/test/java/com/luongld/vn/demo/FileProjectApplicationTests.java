2dc565aebd70d1406314fadc71cf9acacfd66ffd
luong <luongld@vissoft.vn>
2023-01-11 15:03:37 +0700

    first commit

---
+++ src/test/java/com/luongld/vn/demo/FileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luongld.vn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
